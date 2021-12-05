963fafcf071a3be2e4a3cd01e44adad71f29d5a4
sergio portillo <sergiotello83@hotmail.com>
2021-12-04 19:50:50 -0500

    al iniciar app borramos los registros de las tablas

--- src/main/java/com/example/ciclo4_reto3/Ciclo4Reto3Application.java
+++ src/main/java/com/example/ciclo4_reto3/Ciclo4Reto3Application.java
@@ -1,13 +1,37 @@
 package com.example.ciclo4_reto3;
 
+import com.example.ciclo4_reto3.repositories.crud.AccessoryCrudRepository;
+import com.example.ciclo4_reto3.repositories.crud.OrderCrudRepository;
+import com.example.ciclo4_reto3.repositories.crud.SupplementsCrudRepository;
+import com.example.ciclo4_reto3.repositories.crud.UserCrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ciclo4Reto3Application {
 
+    @Autowired
+    private AccessoryCrudRepository crudRepository;
+    @Autowired
+    private UserCrudRepository userCrudRepository;
+    @Autowired
+    private OrderCrudRepository orderCrudRepository;
+    @Autowired
+    private SupplementsCrudRepository supplementsCrudRepository;
+
+
     public static void main(String[] args) {
         SpringApplication.run(Ciclo4Reto3Application.class, args);
+    }
+
+
+    public void run(String... args) throws Exception {
+        crudRepository.deleteAll();
+        userCrudRepository.deleteAll();
+        orderCrudRepository.deleteAll();
+        supplementsCrudRepository.deleteAll();
+
     }
 
 }
